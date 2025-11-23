a3897f3c4fa05abf6a9a9fcdfae3f61bd3a7f2c2
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2025-11-23 23:52:34 +0530

    add favicon icon for website

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,12 @@
-console.log("Alethea System");
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8000";
+
+export const apiClient = axios.create({
+  baseURL: `${API_BASE_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
