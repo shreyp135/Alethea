9842403ceae7ae6e7cd5947162e2f036c6cff242
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2025-11-25 17:32:10 +0530

    fix vercel build

--- client/src/app/(full-width-pages)/(auth)/oauth-callback/page.tsx
+++ client/src/app/(full-width-pages)/(auth)/oauth-callback/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
