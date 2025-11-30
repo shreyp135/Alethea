b9208427533d5b4250a22316f49a5dbee52d90c4
Shreyp135 <shreyanshpaliwalcmsmn@gmail.com>
2025-11-30 16:55:03 +0530

    redeploy

--- client/src/app/(admin)/(others-pages)/pr-analyzer/page.tsx
+++ client/src/app/(admin)/(others-pages)/pr-analyzer/page.tsx
@@ -23,31 +23,7 @@ export default function page() {
 
   // don't render layout until token check runs
   if (token === null) return null;
-  
-  // useEffect(() => {
-  //   const checkGithubLinked = async () => {
-  //     const linked = localStorage.getItem("github_linked");
-  //     if (linked === "true") {
-  //       setIsLinked(true);
-  //       return;
-  //     }
-  //     if (linked === null) {
-  //       if (!token) return;
-  //       const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pr/link`, {
-  //         method: "GET",
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       });
-  //       const data = await response.json();
-  //       localStorage.setItem("github_linked", data.linked.toString());
-  //       setIsLinked(data.linked);
-  //       return;
-  //     }
-  //   };
 
-  //   checkGithubLinked();
-  // }, [token]);
 
   return (
     <div className=" p-6 w-full ">
