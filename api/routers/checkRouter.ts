d91bc272560061e7ee32b8ef2e100da74a126a30
Shreyansh Paliwal <shreyanshpaliwalcmsmn@gmail.com>
2026-07-18 00:56:43 +0530

    add redis ping check for cron job
    Signed-off-by: Shreyansh Paliwal <shreyanshpaliwalcmsmn@gmail.com>

--- api/routers/checkRouter.ts
+++ api/routers/checkRouter.ts
@@ -1,18 +1,31 @@
-import express from "express";
+import express from 'express';
+import { redisClient as redis } from '../../src/utils/redis';
 
 const router = express.Router();
 
-router.get("/", async (_, res) => {
-    try {
-        res.json({
-            status: "ok",
-            timestamp: Date.now(),
-        });
-    } catch {
-        res.status(500).json({
-            status: "error",
-        });
-    }
+router.get('/', async (_, res) => {
+  try {
+    res.json({
+      status: 'ok',
+      timestamp: Date.now(),
+    });
+  } catch {
+    res.status(500).json({
+      status: 'error',
+    });
+  }
+});
+
+router.get('redis', async (_, res) => {
+  try {
+    await redis.ping();
+
+    res.send('OK');
+  } catch {
+    res.status(500).json({
+      status: 'error',
+    });
+  }
 });
 
 export default router;
